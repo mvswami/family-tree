58073580deed492c65beb7dc3e8caf049ee591a7
Linda Nzeukang <lindalauranzeukang@gmail.com>
2021-01-18 11:54:10 -0600

    done

--- family-tree.js
+++ family-tree.js
@@ -15,13 +15,25 @@ class FamilyTree {
   }
   findMember(member) {
     for (let i = 0; i < this.children.length; i++) {
-      console.log(this.children[i].value);
+      //console.log(this.children[i].value);
       if (member === this.children[i].value) {
         return this.children[i];
       }
     }
   }
-  log() {}
+  log() {
+    let newArr = [];
+    newArr.push(`-- ${this.value}`);
+    for (let i = 0; i< this.children.length; i++){
+      newArr.push(`---- ${this.children[i].value}`);
+      for(let j = 0; j < this.children[i].children.length; j++){
+        newArr.push(`------ ${this.children[i].children[j].value}`);
+      }
+
+    }
+    console.log(newArr.join('\n'));
+    return newArr.join('\n');
+  }
 }
 
 module.exports = FamilyTree;
