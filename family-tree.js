2b188c466eabb82434286eef9e5165a303e15349
Russel McMillan <71411627+rfmcmillan@users.noreply.github.com>
2021-01-18 12:11:29 -0500

    completed through findMember

--- family-tree.js
+++ family-tree.js
@@ -1,17 +1,26 @@
 class FamilyTree {
   constructor(value) {
-    if (value === undefined || typeof value !== "string") {
-      throw "Please enter a name!";
+    if (value === undefined || typeof value !== 'string') {
+      throw 'Please enter a name!';
     } else {
       this.value = value;
     }
     this.children = [];
   }
-  insert(children) {
-    this.children.push(new FamilyTree(this.value));
+  insert(child) {
+    this.children.push(new FamilyTree(child));
   }
-  familySize() {}
-  findMember() {}
+  familySize() {
+    return this.children.length + 1;
+  }
+  findMember(member) {
+    for (let i = 0; i < this.children.length; i++) {
+      console.log(this.children[i].value);
+      if (member === this.children[i].value) {
+        return this.children[i];
+      }
+    }
+  }
   log() {}
 }
 
