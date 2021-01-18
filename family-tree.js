6fb66a51a367a1a61ba5f1dc2965df26c3dce267
Manu Swami <swami.manu@gmail.com>
2021-01-18 11:36:12 -0500

    updated files

--- family-tree.js
+++ family-tree.js
@@ -1,7 +1,18 @@
 class FamilyTree {
-  constructor() {}
-
-  //Hello
+  constructor(value) {
+    if (value === undefined || typeof value !== "string") {
+      throw "Please enter a name!";
+    } else {
+      this.value = value;
+    }
+    this.children = [];
+  }
+  insert(children) {
+    this.children.push(new FamilyTree(this.value));
+  }
+  familySize() {}
+  findMember() {}
+  log() {}
 }
 
 module.exports = FamilyTree;
